7748d88cd54862ef3faea3032d3d626b9d383df9
bd-web-dev <isnain.cmd@outlook.com>
2024-09-09 07:02:47 +0600

    Module 18.5 Video 1: Array as storage area for multiple values under a single variable name.

---
+++ array.js
@@ -0,0 +1,21 @@
+const num1 = 12;
+const num2 = 22;
+const num3 = 442;
+const num4 = 442;
+
+// WRONG:
+// const numbers = 1222442442;
+
+// 3 parts of setting an array value to variable
+// 1. 3rd or square brackets
+// 2. Commas separating the different values
+// 3. (Not mandatory) Space between the different values
+
+// Array storing Number vlues
+const numbers = [12, 22, 442, 442];
+// Array storing String values
+const friends = ["Abul", "Babul", "Cabul", "Dabul"];
+// Array storing Boolean values
+const passed = [true, false, true, false];
+// Array storing multiple types of values
+const mixedTypes = [12, "String", true];
